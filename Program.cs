136e50820a340c330ba0e62c48bbf3f23bf69c3f
Piers Karsenbarg <piers@pulumi.com>
2025-07-16 15:41:46 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Pulumi;
+using Pulumi.Local;
+
+return await Deployment.RunAsync(() =>
+{
+    var foo = GetFile.Invoke(new()
+    {
+        Filename = "./Pulumi.yaml",
+    });
+
+
+    // Export outputs here
+    return new Dictionary<string, object?>
+    {
+        ["outputKey"] = "outputValue"
+    };
+});
